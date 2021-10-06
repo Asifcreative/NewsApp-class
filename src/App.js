6b0740ad4eca158ee9b96134521b471d2a41eae2
Aasif Javed <asifgulu88@gmail.com>
2021-10-06 23:12:47 +0500

    second

--- src/App.js
+++ src/App.js
@@ -8,27 +8,13 @@ import NewsMarquee from './components/NewsMarquee';
 import NewsSlider from './components/NewsSlider';
 
 const App = () => {
-  const pageSize = 8;
-  // const apiKey = process.env.REACT_APP_NEWS_API
-  const [progress, setProgress] = useState(0);
-  const [country, setCountry] = useState('us');
   return (
     <div>
       <Router>
         <NavBar class="nav-link active" />
         <NewsMarquee/>
-        <Switch>
-          <Route exact path="/"><NewsSlider/></Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/"><News/></Route>
-          <Route exact path="/business"><News/></Route>
-          <Route exact path="/entertainment"><News/></Route>
-          <Route exact path="/health"><News/></Route>
-          <Route exact path="/science"><News/></Route>
-          <Route exact path="/sports"><News/></Route>
-          <Route exact path="/technology"><News/></Route>
-        </Switch>
+        <NewsSlider/>
+        <News/>
         <Footer />
 
       </Router>
